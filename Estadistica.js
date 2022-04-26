32f4d29a77fe205759f9fab7344bc94f86c6f9d7
fdcorrea12 <becasfabian@hotmail.com>
2022-04-26 16:33:20 -0500

    Estadistica final

--- Estadistica.js
+++ Estadistica.js
@@ -7,9 +7,17 @@ function Capturar(){
     for (var i = 0; i < lstNumero.length; i++) {    
         arrayGuardar[i] = lstNumero[i].value;
         datos.push(parseInt(arrayGuardar[i], 10));     
-    }   
+    } 
 }
 
+//Imprimir datos que son ingresados por el usuario
+ 
+function datosIngresados(){
+    const ingresados = Capturar();
+    const ingresoDatos = document.getElementById("datosIngresados");
+    ingresoDatos.innerText = "Datos inscritos por el Usuario: " + ingresados + ", ";
+    return ingresoDatos; 
+}
 
 // promedio
 function calcularMediaAritmetica(lista){
@@ -58,4 +66,42 @@ function moda(){
     const result = document.getElementById("resultFinal");
     result.innerText = "el promedio de los valores ingresados es de: " + moda;
     return moda;
+}
+
+//Mediana
+function calcularMediana(lista){
+    lista.sort(function(elemento1, elemento2){
+        return elemento1 -elemento2
+    });
+
+    const mitadLista1 = parseInt(lista.length / 2);
+
+    //Si la lista es par o impar
+    function esPar(numerito){
+        if(numerito % 2 === 0){
+            return true;
+        }else{
+            return false;
+        };
+            
+    };
+    let mediana;
+    if(esPar(lista.length)){
+        const elemento1 = lista[mitadLista1 - 1];
+        const elemento2 = lista[mitadLista1];
+
+        const elemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+
+        mediana = elemento1y2;
+    }else{
+        mediana = lista[mitadLista1];
+    };
+    return mediana;
+}
+
+function mediana(){
+    const mediana = calcularMediana(datos);
+    const result = document.getElementById("resultFinal");
+    result.innerText = "El Valor de la mediana es: " + mediana;
+    return mediana;
 }
